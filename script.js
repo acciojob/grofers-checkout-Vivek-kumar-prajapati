bf4d1263d7d80da76bdbe78f0198a883a2962582
Vivek-kumar-prajapati <148374468+Vivek-kumar-prajapati@users.noreply.github.com>
2026-08-16 13:02:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const getSumBtn = document.createElement("button");
+const total =document.querySelector(".total");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
  let pricelist= document.querySelectorAll(".price");
-	console.log(pricelist)
+	let sum=0;
+    for(let item of pricelist){
+		 sum+=Number(item.value);
+		
+	}
+   total.TextContent=sum;
+	
   
 };
 
